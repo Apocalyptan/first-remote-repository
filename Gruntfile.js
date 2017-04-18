963018c8d098f08769b8a352b685153194439d93
Adam Rudny <rudnyadam@gmail.com>
2017-04-18 11:54:51 +0200

    changes

---
+++ Gruntfile.js
@@ -0,0 +1,12 @@
+module.exports = function(grunt) {
+	
+	//Project configuration.
+	grunt.initConfig({
+
+
+	});
+	// Load the plugin tasks
+
+	// Default task(s).
+	grunt.registerTask('default'. []);
+};
